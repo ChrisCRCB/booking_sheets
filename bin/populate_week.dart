52198b3010730c77ba81c8529077a48659997122
Chris Norman CRCB <chris@crcb.org.uk>
2025-04-04 11:38:19 +0100

    Create 2 weeks in advance.

--- bin/populate_week.dart
+++ bin/populate_week.dart
@@ -11,69 +11,78 @@ void main(final List<String> arguments) {
     mastersDirectory = Directory.current;
   }
   final now = DateTime.now();
-  final DateTime startDate;
-  if (now.weekday > 1) {
-    startDate = now.add(Duration(days: 8 - now.weekday));
-  } else {
-    startDate = now;
-  }
-  final weekNumber = (startDate.day / 7).floor() + 1;
-  final baseDirectory = mastersDirectory.parent;
-  final year = startDate.year;
-  final month = startDate.month.toString().padLeft(2, '0');
-  final day = startDate.day.toString().padLeft(2, '0');
-  final yearDirectory = Directory(
-    path.join(baseDirectory.path, 'Booking Sheets $year'),
-  );
-  final weekDirectory = Directory(
-    path.join(yearDirectory.path, 'Booking Sheets (wc $year-$month-$day)'),
-  );
-  if (!weekDirectory.existsSync()) {
-    weekDirectory.createSync(recursive: true);
-  }
   final filenameRegExp = RegExp(
     r'([^-])-(\d\d)(\d\d)([.]([^ ]+)| )([^.]+).xlsx',
   );
-  for (final file in mastersDirectory.listSync().whereType<File>()) {
-    final basename = path.basename(file.path);
-    final match = filenameRegExp.firstMatch(basename);
-    if (match == null) {
-      continue;
+  final DateTime start;
+  if (now.weekday > 1) {
+    start = now.add(Duration(days: 8 - now.weekday));
+  } else {
+    start = now;
+  }
+  final baseDirectory = mastersDirectory.parent;
+  for (var i = 0; i < 2; i++) {
+    final DateTime date;
+    if (i == 0) {
+      date = start;
+    } else {
+      date = start.add(Duration(days: i));
     }
-    final dayGroup = match.group(1)!;
-    final weekGroup = match.group(5);
-    if (weekGroup != null) {
-      final groupWeekNumber = int.parse(weekGroup);
-      if (weekNumber != groupWeekNumber) {
+    final weekNumber = (date.day / 7).floor() + 1;
+    final year = date.year;
+    final month = date.month.toString().padLeft(2, '0');
+    final day = date.day.toString().padLeft(2, '0');
+    final yearDirectory = Directory(
+      path.join(baseDirectory.path, 'Booking Sheets $year'),
+    );
+    final weekDirectory = Directory(
+      path.join(yearDirectory.path, 'Booking Sheets (wc $year-$month-$day)'),
+    );
+    if (!weekDirectory.existsSync()) {
+      weekDirectory.createSync(recursive: true);
+    }
+    for (final file in mastersDirectory.listSync().whereType<File>()) {
+      final basename = path.basename(file.path);
+      final match = filenameRegExp.firstMatch(basename);
+      if (match == null) {
         continue;
       }
-    }
-    final destination = File(path.join(weekDirectory.path, basename));
-    if (!destination.existsSync()) {
-      final bytes = file.readAsBytesSync();
-      final excel = Excel.decodeBytes(bytes);
-      final sheet = excel.sheets[excel.sheets.keys.first];
-      final a1 = sheet!.rows.first.first;
-      if (a1 != null) {
-        throw StateError('A1 should be blank, not $a1.');
+      final dayGroup = match.group(1)!;
+      final weekGroup = match.group(5);
+      if (weekGroup != null) {
+        final groupWeekNumber = int.parse(weekGroup);
+        if (weekNumber != groupWeekNumber) {
+          continue;
+        }
       }
-      final DateTime groupStartDate;
-      final groupDay = int.parse(dayGroup);
-      if (groupDay == 1) {
-        // Start date always falls on a Monday.
-        groupStartDate = startDate;
-      } else {
-        groupStartDate = startDate.add(Duration(days: groupDay - 1));
+      final destination = File(path.join(weekDirectory.path, basename));
+      if (!destination.existsSync()) {
+        final bytes = file.readAsBytesSync();
+        final excel = Excel.decodeBytes(bytes);
+        final sheet = excel.sheets[excel.sheets.keys.first];
+        final a1 = sheet!.rows.first.first;
+        if (a1 == null) {
+          throw StateError('A1 should not be blank, not $a1.');
+        }
+        final DateTime groupStartDate;
+        final groupDay = int.parse(dayGroup);
+        if (groupDay == 1) {
+          // Start date always falls on a Monday.
+          groupStartDate = date;
+        } else {
+          groupStartDate = date.add(Duration(days: groupDay - 1));
+        }
+        sheet.updateCell(
+          a1.cellIndex,
+          DateCellValue(
+            year: groupStartDate.year,
+            month: groupStartDate.month,
+            day: groupStartDate.day,
+          ),
+          cellStyle: a1.cellStyle,
+        );
+        destination.writeAsBytesSync(excel.save()!);
       }
-      sheet.updateCell(
-        CellIndex.indexByString('a1'),
-        DateCellValue(
-          year: groupStartDate.year,
-          month: groupStartDate.month,
-          day: groupStartDate.day,
-        ),
-      );
-      destination.writeAsBytesSync(excel.save()!);
     }
   }
 }
